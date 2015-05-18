10332f7009f1603023713c63589f6429db644f1d
Damien <damien@sail>
2015-05-18 22:18:56 +0930

    - Feeds now return an Enumerator (implemented for Atom)  - Implemented caches so you don't fetch images you already have  - Download the image content from Atom feeds

--- lib/Atom.rb
+++ lib/Atom.rb
@@ -3,36 +3,35 @@ require 'atom'
 
 module ImageFeed
   
-  def self.parse(src, since=nil)
-    feed = Atom::Feed.load_feed(src)
-    results = []
-    feed.entries.select do |entry|
-      # feed.each_entry has a "since" parameter, but Flickr's feeds look
-      # like they arrive in the wrong order for it
-      since == nil or since < entry.published
-    end .collect do |entry|
-      results << {
-        :id => entry.id,
-        :caption => entry.title,
-        :date => entry.published,
-        :src => entry.links.enclosures.first.href,
-        :link => entry.links.alternate.href
-      }
-    end
-    results
-  end
-  
-  class RSS
+  class Atom
     
-    def initialize(url)
-      @url = url
+    def initialize(src)
+      @src = src.is_a?(IO) && src || open(src)
     end
   
-    def update(since)
-      parse(open($url), since)
+    def each(since=nil, cache={})
+      return enum_for(:each, since, cache) unless block_given?
+      
+      feed = ::Atom::Feed.load_feed(@src)
+      feed.entries.select do |entry|
+        # feed.each_entry has a "since" parameter, but Flickr's feeds look
+        # like they arrive in the wrong order for it
+        since == nil || (!cache.has_key?(entry.id) && since < entry.published)
+      end .collect do |entry|
+        content = open(entry.links.enclosures.first.href).read
+        yield({
+          :id => entry.id,
+          :caption => entry.title,
+          :date => entry.published,
+          :src => entry.links.enclosures.first.href,
+          :link => entry.links.alternate.href,
+          :image => content,
+          :type => content.content_type
+        })
+      end
+
     end
-    
+
   end
-  
     
 end
--- tests/test_Atom.rb
+++ tests/test_Atom.rb
@@ -1,29 +1,57 @@
 require "Atom.rb"
-require 'test/unit'
+require 'minitest/autorun'
 require 'time'
+require 'stringio'
 
-class Test_Atom < Test::Unit::TestCase
+class Test_Atom <  Minitest::Test
 
-  @@content = open('tests/test_Atom_flickr.xml')
-  @@feed = ImageFeed::parse @@content
-
+  def setup
+    @feed = ImageFeed::Atom.new open('tests/test_Atom_flickr.xml')
+    def @feed.open(f)
+      r = StringIO.new f, 'r'
+      def r.read
+        # Call the StringIO method in the only way I know how!
+        str = self.class.instance_method(:read).bind(self).call()
+        def str.content_type()
+          'image/jpeg'
+        end
+        str
+      end
+      r
+    end
+  end
+  
   def test_read_all()
-    assert_equal 20, @@feed.length 
+    results = @feed.each.to_a
+    assert_equal 20, results.length
   end
     
   def test_read_since()
-    r = ImageFeed::parse open('tests/test_Atom_flickr.xml'), Time.parse('2015-04-27T17:46:24Z')
-    assert_equal 3, r.length
+    feed = ImageFeed::Atom.new open('tests/test_Atom_flickr.xml')
+    results = @feed.each(Time.parse('2015-04-27T17:46:24Z')).to_a
+    assert_equal 3, results.length
   end
   
   def test_read_item()
-    require 'time'
-    item = @@feed.first
+    item = @feed.each.to_a.first
     assert_equal 'tag:flickr.com,2005:/photo/17105504949', item[:id]
     assert_equal "It's green !!", item[:caption]
     assert_equal Time.parse('2015-04-27T17:50:01Z'), item[:date]
     assert_equal 'https://farm8.staticflickr.com/7661/17105504949_6b9e595105_b.jpg', item[:src]
     assert_equal 'https://www.flickr.com/photos/hameem15/17105504949/', item[:link]
+  end
+  
+  def test_cache_with_since()
+    puts '==============='
+    results = @feed.each(Time.parse('2015-04-27T17:38:50Z'), {
+      'tag:flickr.com,2005:/photo/17084261817' => 1,
+      'tag:flickr.com,2005:/photo/17291685035' => 1
+    })
+    
+    # Extract the IDs
+    results = Hash[results.collect { |e| [e[:id], e] }]
+    assert_equal 5, results.length
+    
   end
   
 end
